4038eeaeaf1a83b29e1def080fb26e035c4f0ca2
alexandre <alexandre020602@gmail.com>
2026-09-05 20:58:16 -0300

    feat: add Avatar and DropdownMenu components with user profile functionality

--- components/Header/HeaderButtons.tsx
+++ components/Header/HeaderButtons.tsx
@@ -1,8 +1,18 @@
 import { Button } from "../ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { ChevronDown } from 'lucide-react';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 const HeaderButtons = () => {
 
-    const isLoggedIn: boolean = false
+    const isLoggedIn: boolean = true
 
     return (
         <>
@@ -23,8 +33,41 @@ const HeaderButtons = () => {
                     </Button>
                 </div>
             ) : (
-                <div>
-                    <p>Alexandre</p>
+                <div className="p-4 border-3 border-black min-w-50">
+                    <div className="flex items-center justify-between">
+
+                        <div className="flex items-center gap-2">
+                            <Avatar>
+                                <AvatarImage
+                                    src="https://github.com/shadcn.png"
+                                    alt="@shadcn"
+                                />
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                            <div>
+                                <p>Unfast</p>
+                            </div>
+                        </div>
+
+                        <div>
+                            <DropdownMenu>
+                                <DropdownMenuTrigger style={{ cursor: "pointer" }}>
+                                    <ChevronDown />
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent align="end" sideOffset={25} className={"bg-background w-50 -mr-4.5 rounded-none p-4"}>
+                                    <DropdownMenuGroup>
+                                        <DropdownMenuItem className={"cursor-pointer"}>👤 Meu Perfil</DropdownMenuItem>
+                                        <DropdownMenuSeparator />
+                                        <DropdownMenuItem className={"cursor-pointer"}>📋 Minha Lista</DropdownMenuItem>
+                                        <DropdownMenuSeparator />
+                                        <DropdownMenuItem className={"cursor-pointer"}>⚙️ Configurações</DropdownMenuItem>
+                                        <DropdownMenuSeparator />
+                                        <DropdownMenuItem className={"cursor-pointer text-red"}>↪ Sair</DropdownMenuItem>
+                                    </DropdownMenuGroup>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        </div>
+                    </div>
                 </div>
             )}
         </>
