18dc59bca122551b0822ae09fcbb81b32fd343ab
alexandre <alexandre020602@gmail.com>
2026-09-14 22:54:42 -0300

    refactor: remove LoginBackground component and associated video file

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,8 +1,9 @@
-import LoginBackground from '@/components/auth/LoginBackground'
+
+
 const page = () => {
   return (
     <main className='relative w-screen min-h-screen flex items-center justify-center px-5 overflow-hidden z-0'>
-      <LoginBackground />
+
     </main>
   )
 }
--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -1,20 +1,9 @@
-"use client";
-
-import { useState } from "react"
-
 
 export const LoginForm = () => {
 
-    const [email, setEmail] = useState<string>();
-    const [password, setPassword] = useState<string>();
-
-    const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        console.log({ email, password });
-    }
-
-
     return (
-        <></>
+        <>
+
+        </>
     )
 }
