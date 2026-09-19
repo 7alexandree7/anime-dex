33ddbe55e1e1d489b720f2fd8a6fcd63e43d9f28
alexandre <alexandre020602@gmail.com>
2026-09-19 10:55:20 -0300

    feat: create ExplorePage component and update LandingPage for session handling

---
+++ app/(main)/explore/page.tsx
@@ -0,0 +1,11 @@
+
+
+const ExplorePage = () => {
+  return (
+    <div>
+      <h1>explore</h1>
+    </div>
+  )
+}
+
+export default ExplorePage
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,8 +1,18 @@
 import FeaturePanels from "@/components/landing/FeaturePanels";
 import Footer from "@/components/landing/Footer";
 import Hero from "@/components/landing/Hero";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function LandingPage() {
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session) redirect('/explore');
+  
   return (
     <>
       <Hero />
