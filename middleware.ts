d1ff201714b1a17fe3506406636c3281953d7db7
alexandre <alexandre020602@gmail.com>
2026-09-20 15:25:18 -0300

    feat: create DashboardPage component and update middleware for redirect handling

--- middleware.ts
+++ middleware.ts
@@ -4,21 +4,28 @@ import type { NextRequest } from "next/server";
 export async function middleware(request: NextRequest) {
   // 1. Pegue o cookie de sessão que o Better Auth cria
   const sessionToken = request.cookies.get("better-auth.session_token");
-  
+
   const { pathname } = request.nextUrl;
 
   // 2. Se tentar entrar no /dashboard OU no /explore e NÃO estiver logado, manda para o /login
   if ((pathname.startsWith("/dashboard") || pathname.startsWith("/explore")) && !sessionToken) {
-    return NextResponse.redirect(new URL("/login", request.url));
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  // 3. Se tentar entrar no /login OU no /register e estiver logado, manda para a Landing Page
+  if ((pathname.startsWith("/login") || pathname.startsWith("/register")) && sessionToken) {
+    return NextResponse.redirect(new URL("/explore", request.url))
   }
 
   return NextResponse.next();
 }
 
 // Configura quais rotas o Middleware deve monitorar de verdade
-export const config = {
+export const config = {  // 
   matcher: [
     "/dashboard/:path*",
-    "/explore/:path*" // 💡 Adicionado para o Next.js interceptar essa rota também
-  ], 
+    "/explore/:path*",  //💡 Adicionado para o Next.js interceptar essa rota também
+    "/login/:path*",
+    "/register/:path*",
+  ],
 };
